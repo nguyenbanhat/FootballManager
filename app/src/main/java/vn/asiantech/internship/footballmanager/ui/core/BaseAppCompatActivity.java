fe165dcbf97e191c5e905432a4dfd5df9d699615
nguyenbanhat <crack.item.92@gmail.com>
2015-10-20 10:04:42 +0700

    run annotation

---
+++ app/src/main/java/vn/asiantech/internship/footballmanager/ui/core/BaseAppCompatActivity.java
@@ -0,0 +1,7 @@
+package vn.asiantech.internship.footballmanager.ui.core;
+
+/**
+ *  Created by sunday on 20/10/2015.
+ */
+public class BaseAppCompatActivity {
+}
---
+++ app/src/main/java/vn/asiantech/internship/footballmanager/ui/league/MainActivity.java
@@ -0,0 +1,17 @@
+package vn.asiantech.internship.footballmanager.ui.league;
+
+import android.support.v7.app.AppCompatActivity;
+
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.EActivity;
+
+import vn.asiantech.internship.footballmanager.R;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends AppCompatActivity {
+    @AfterViews
+    public void afterView(){
+        //TODO something;
+    }
+
+}
